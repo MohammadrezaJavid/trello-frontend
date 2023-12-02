d931804c5b8904599d67a3f18dd04dcb18baa494
mohammadrezajavid <javid.9@yahoo.com>
2023-12-02 13:11:51 +0330

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,23 @@
 <template>
-  <p>{{ wellcomeMassage }}</p>
-  
-  <br>
-  <br>
-  <router-link to="/signup">SignUp</router-link>
-  <br>
-  <router-link to="/login">Login</router-link>
-  <br>
-  <router-link to="/logout">Logout</router-link>
-  <br>
-  <br>
-  <router-link to="/trello/boards">Boards</router-link>
-  <br>
-  <router-link to="/trello/lists">Lists</router-link>
-  <br>
-  <router-link to="/trello/cards">Cards</router-link>
-  <br>
-  <br>
-  <br>
-
-  <router-link to="/trello/board">CreateBoard</router-link>
-  <br>
-  <router-link to="/trello/list">CreateList</router-link>
-  <br>
-  <router-link to="/trello/card">CreateCard</router-link>
-  <br>
-  <br>
-  <br>
-
-  <router-link to="/trello/delete/board">DeleteBoard</router-link>
-  <br>
-  <router-link to="/trello/delete/list">DeleteList</router-link>
-  <br>
-  <router-link to="/trello/delete/card">DeleteCard</router-link>
-  <br>
-  <div>
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import { useStore } from 'vuex';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
-  setup() {
-    const store = useStore();
-    const wellcomeMassage = store.state.wellcomeMassage;
+  name: 'App',
 
-    return { wellcomeMassage };
+  components: {
+    HelloWorld,
   },
-};
+
+  data: () => ({
+    //
+  }),
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,54 +1,12 @@
-import './assets/main.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import App from './App.vue';
-import axios from 'axios';
+loadFonts()
 
-import store from './store';
-
-import Signup from './components/SignupPage.vue'
-import Login from './components/LoginPage.vue';
-import Logout from './components/LogoutPage.vue';
-
-
-import BoardList from './components/BoardList.vue';
-import ListList from './components/ListList.vue';
-import CardList from './components/CardList.vue';
-
-import CreateBoard from './components/CreateBoard.vue';
-import CreateList from './components/CreateList.vue';
-import CreateCard from './components/CreateCard.vue';
-
-import DeleteBoard from './components/DeleteBoard.vue';
-import DeleteList from './components/DeleteList.vue';
-import DeleteCard from './components/DeleteCard.vue';
-
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/signup', component:  Signup},
-        { path: '/login', component:  Login},
-        { path: '/logout', component:  Logout},
-
-        { path: '/trello/boards', component:  BoardList},
-        { path: '/trello/lists', component: ListList },
-        { path: '/trello/cards', component: CardList },
-        
-        { path: '/trello/board', component: CreateBoard },
-        { path: '/trello/list', component: CreateList },
-        { path: '/trello/card', component: CreateCard },
-
-        { path: '/trello/delete/board', component: DeleteBoard },
-        { path: '/trello/delete/list', component: DeleteList },
-        { path: '/trello/delete/card', component: DeleteCard },
-    ]
-});
-
-const app = createApp(App);
-
-app.config.globalProperties.$http = axios;
-app.use(store);
-app.use(router);
-app.mount('#app');
+createApp(App)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
